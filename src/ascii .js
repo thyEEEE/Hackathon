ac97e59297336270c0e3a7a26e9577b8e7fe28b1
thyEEEE <96706543+thyEEEE@users.noreply.github.com>
2022-01-14 09:04:59 +0800

    Add files via upload

---
+++ src/ascii .js
@@ -0,0 +1,123 @@
+    const A = "░█████╗░\n██╔══██╗\n███████║\n██╔══██║\n██║░░██║\n╚═╝░░╚═╝\n"
+    const B = "██████╗░\n██╔══██╗\n██████╦╝\n██╔══██╗\n██████╦╝\n╚═════╝░\n"
+    const C = "░█████╗░\n██╔══██╗\n██║░░╚═╝\n██║░░██╗\n╚█████╔╝\n░╚════╝░\n"
+    const D = "██████╗░\n██╔══██╗\n██║░░██║\n██║░░██║\n██████╔╝\n╚═════╝░\n"
+    const E = "███████╗\n██╔════╝\n█████╗░░\n██╔══╝░░\n███████╗\n╚══════╝\n"
+    const F = "███████╗\n██╔════╝\n█████╗░░\n██╔══╝░░\n██║░░░░░\n╚═╝░░░░░\n"
+    const G = "░██████╗\n██╔════╝░\n██║░░██╗░\n██║░░╚██╗\n╚██████╔╝\n░╚═════╝░\n"
+    const H = "██╗░░██╗\n██║░░██║\n███████║\n██╔══██║\n██║░░██║\n╚═╝░░╚═╝\n"
+    const I = "██╗\n██║\n██║\n██║\n██║\n╚═╝\n"
+    const J = "░░░░░██╗\n░░░░░██║\n░░░░░██║\n██╗░░██║\n╚█████╔╝\n░╚════╝░\n"
+    const K = "██╗░░██╗\n██║░██╔╝\n█████═╝░\n██╔═██╗░\n██║░╚██╗\n╚═╝░░╚═╝\n"
+    const L = "██╗░░░░░\n██║░░░░░\n██║░░░░░\n██║░░░░░\n███████╗\n╚══════╝\n"
+    const M = "███╗░░░███╗\n████╗░████║\n██╔████╔██║\n██║╚██╔╝██║\n██║░╚═╝░██║\n╚═╝░░░░░╚═╝\n"
+    const N = "███╗░░██╗\n████╗░██║\n██╔██╗██║\n██║╚████║\n██║░╚███║\n╚═╝░░╚══╝\n"
+    const O = "░█████╗░\n██╔══██╗\n██║░░██║\n██║░░██║\n╚█████╔╝\n░╚════╝░\n"
+    const P = "██████╗░\n██╔══██╗\n██████╔╝\n██╔═══╝░\n██║░░░░░\n╚═╝░░░░░\n"
+    const Q = "░██████╗░\n██╔═══██╗\n██║██╗██║\n╚██████╔╝\n░╚═██╔═╝░\n░░░╚═╝░░░\n"
+    const R = "██████╗░\n██╔══██╗\n██████╔╝\n██╔══██╗\n██║░░██║\n╚═╝░░╚═╝\n"
+    const S = "░██████╗\n██╔════╝\n╚█████╗░\n░╚═══██╗\n██████╔╝\n╚═════╝░\n"
+    const T = "████████╗\n╚══██╔══╝\n░░░██║░░░\n░░░██║░░░\n░░░██║░░░\n░░░╚═╝░░░\n"
+    const U = "██╗░░░██╗\n██║░░░██║\n██║░░░██║\n██║░░░██║\n╚██████╔╝\n░╚═════╝░\n"
+    const V = "██╗░░░██╗\n██║░░░██║\n╚██╗░██╔╝\n░╚████╔╝░\n░░╚██╔╝░░\n░░░╚═╝░░░\n"
+    const W = "░██╗░░░░░░░██╗\n░██║░░██╗░░██║\n░╚██╗████╗██╔╝\n░░████╔═████║░\n░░╚██╔╝░╚██╔╝░\n░░░╚═╝░░░╚═╝░░\n"
+    const X = "██╗░░██╗\n╚██╗██╔╝\n░╚███╔╝░\n░██╔██╗░\n██╔╝╚██╗\n╚═╝░░╚═╝\n"
+    const Y = "██╗░░░██╗\n╚██╗░██╔╝\n░╚████╔╝░\n░░╚██╔╝░░\n░░░██║░░░\n░░░╚═╝░░░\n"
+    const Z = "███████╗\n╚════██║\n░░███╔═╝\n██╔══╝░░\n███████╗\n╚══════╝\n"
+    /*             type 2 :                */
+    const A2 = "▄▀█\n█▀█\n"
+    const B2 = "█▄▄\n█▄█\n"
+    const C2 = "█▀▀\n█▄▄\n"
+    const D2 = "█▀▄\n█▄▀\n"
+    const E2 = "█▀▀\n██▄\n"
+    const F2 = "█▀▀\n█▀░\n"
+    const G2 = "█▀▀\n█▄█\n"
+    const H2 = "█░█\n█▀█\n"
+    const I2 = "█\n█\n"
+    const J2 = "░░█\n█▄█\n"
+    const K2 = "█▄▀\n█░█\n"
+    const L2 = "█░░\n█▄▄\n"
+    const M2 = "█▀▄▀█\n█░▀░█\n"
+    const N2 = "█▄░█\n█░▀█\n"
+    const O2 = "█▀█\n█▄█\n"
+    const P2 = "█▀█\n█▀▀\n"
+    const Q2 = "█▀█\n▀▀█\n"
+    const R2 = "█▀█\n█▀▄\n"
+    const S2 = "█▀\n▄█\n"
+    const T2 = "▀█▀\n░█░\n"
+    const U2 = "█░█\n█▄█\n"
+    const V2 = "█░█\n▀▄▀\n"
+    const W2 = "█░█░█\n▀▄▀▄▀\n"
+    const X2 = "▀▄▀\n█░█\n"
+    const Y2 = "█▄█\n░█░\n"
+    const Z2 = "▀█\n█▄\n"
+    /*                random shit                  */
+/*<-------------------------------  Super Cool Filter Part  --------------------------------------->*/ 
+function convert(toconvert,types) {
+    if (types > 2){
+        return "there is only two types uwu\n(1,2)"
+    }
+    if (types === 1||!types){
+        replaceA = toconvert.toUpperCase().replace(/A/g,A)
+        replaceB = replaceA.replace(/B/g,B)
+        replaceC = replaceB.replace(/C/g,C)
+        replaceD = replaceC.replace(/D/g,D)
+        replaceE = replaceD.replace(/E/g,E)
+        replaceF = replaceE.replace(/F/g,F)
+        replaceG = replaceF.replace(/G/g,G)
+        replaceH = replaceG.replace(/H/g,H)
+        replaceI = replaceH.replace(/I/g,I)
+        replaceJ = replaceI.replace(/J/g,J)
+        replaceK = replaceJ.replace(/K/g,K)
+        replaceL = replaceK.replace(/L/g,L)
+        replaceM = replaceL.replace(/M/g,M)
+        replaceN = replaceM.replace(/N/g,N)
+        replaceO = replaceN.replace(/O/g,O)
+        replaceP = replaceO.replace(/P/g,P)
+        replaceQ = replaceP.replace(/Q/g,Q)
+        replaceR = replaceQ.replace(/R/g,R)
+        replaceS = replaceR.replace(/S/g,S)
+        replaceT = replaceS.replace(/T/g,T)
+        replaceU = replaceT.replace(/U/g,U)
+        replaceV = replaceU.replace(/V/g,V)
+        replaceW = replaceV.replace(/W/g,W)
+        replaceX = replaceW.replace(/X/g,X)
+        replaceY = replaceX.replace(/Y/g,Y)
+        replaceZ = replaceY.replace(/Z/g,Z)
+        replaceSpace = replaceZ.replace(/ /g,"\n\n")
+        return replaceSpace;
+    }
+    if (types === 2){
+        replaceA2 = toconvert.toUpperCase().replace(/A/g,A2)
+        replaceB2 = replaceA2.replace(/B/g,B2)
+        replaceC2 = replaceB2.replace(/C/g,C2)
+        replaceD2 = replaceC2.replace(/D/g,D2)
+        replaceE2 = replaceD2.replace(/E/g,E2)
+        replaceF2 = replaceE2.replace(/F/g,F2)
+        replaceG2 = replaceF2.replace(/G/g,G2)
+        replaceH2 = replaceG2.replace(/H/g,H2)
+        replaceI2 = replaceH2.replace(/I/g,I2)
+        replaceJ2 = replaceI2.replace(/J/g,J2)
+        replaceK2 = replaceJ2.replace(/K/g,K2)
+        replaceL2 = replaceK2.replace(/L/g,L2)
+        replaceM2 = replaceL2.replace(/M/g,M2)
+        replaceN2 = replaceM2.replace(/N/g,N2)
+        replaceO2 = replaceN2.replace(/O/g,O2)
+        replaceP2 = replaceO2.replace(/P/g,P2)
+        replaceQ2 = replaceP2.replace(/Q/g,Q2)
+        replaceR2 = replaceQ2.replace(/R/g,R2)
+        replaceS2 = replaceR2.replace(/S/g,S2)
+        replaceT2 = replaceS2.replace(/T/g,T2)
+        replaceU2 = replaceT2.replace(/U/g,U2)
+        replaceV2 = replaceU2.replace(/V/g,V2)
+        replaceW2 = replaceV2.replace(/W/g,W2)
+        replaceX2 = replaceW2.replace(/X/g,X2)
+        replaceY2 = replaceX2.replace(/Y/g,Y2)
+        replaceZ2 = replaceY2.replace(/Z/g,Z2)
+        replaceSpace2 = replaceZ2.replace(/ /g,"\n")
+        return replaceSpace2;
+    }
+}
+/*<---------------------------    End Of Super Cool Filter Part   ------------------------------------------->*/
+
+console.log(convert("uwu",1))
